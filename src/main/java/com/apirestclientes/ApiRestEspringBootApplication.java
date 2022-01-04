338c6bf69d51ec8cd57f91011516cfa41ebc3b40
kevinAnaya <kevinanaya278@gmail.com>
2022-01-04 14:45:54 -0500

    firts commit

---
+++ src/main/java/com/apirestclientes/ApiRestEspringBootApplication.java
@@ -0,0 +1,13 @@
+package com.apirestclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestEspringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestEspringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apirestclientes/ApiRestEspringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirestclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestEspringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
